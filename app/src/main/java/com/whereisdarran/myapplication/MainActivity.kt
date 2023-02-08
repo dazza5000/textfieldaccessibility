53453d5445dad6fdddacbc7973ade534d9e96a10
Darran Kelinske <darran.kelinske@gmail.com>
2023-02-08 13:21:38 -0600

    implement error message

--- app/src/main/java/com/whereisdarran/myapplication/MainActivity.kt
+++ app/src/main/java/com/whereisdarran/myapplication/MainActivity.kt
@@ -54,29 +54,21 @@ class MainActivity : ComponentActivity() {
                             OutlinedTextField(
                                 value = value.value,
                                 { value.value = it },
-//                                modifier = Modifier.semantics(mergeDescendants = true) {
-//                                    if (isError.value) liveRegion = LiveRegionMode.Polite
-//                                },
                                 isError = isError.value,
-                                label =  { if (isError.value) Text(error.value) else null }
-                            )
-                            TextFieldSample(
-                                value = TextFieldValue(""),
-                                onValueChange = {
-                                    value.value = it
-                                },
-                                label = "text field test two"
-                            )
-                            TextFieldSample(
-                                value = TextFieldValue(""),
-                                onValueChange = {
-                                    value.value = it
-                                },
-                                label = "text field test three"
                             )
                         }
+                        if (error.value.isNotBlank()) {
+                                Text(
+                                    text = error.value,
+                                    modifier = Modifier
+                                        .padding(top = 2.dp, start = 16.dp)
+                                        .semantics {
+//                                            liveRegion = LiveRegionMode.Polite
+                                        }
+                                )
+                        }
                         Button({
-                            isError.value = !isError.value
+                            isError.value = true
                             error.value = if (isError.value) "please complete this field" else ""
                         }) {
                             Text("Button")
@@ -101,171 +93,3 @@ fun DefaultPreview() {
     }
 }
 
-@Composable
-fun TextFieldSample(
-    modifier: Modifier = Modifier,
-    value: TextFieldValue,
-    onValueChange: (TextFieldValue) -> Unit,
-    enabled: Boolean = true,
-    readOnly: Boolean = false,
-    textStyle: TextStyle = LocalTextStyle.current,
-    label: String,
-    placeholder: @Composable (() -> Unit)? = null,
-    trailingIcon: @Composable (() -> Unit)? = null,
-    errorMessage: String? = null,
-    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
-    lines: Int = 1,
-    isOutlinedTextField: Boolean = true,
-    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-    autofillTypes: List<AutofillType> = listOf(),
-    colors: TextFieldColors = TextFieldDefaults.outlinedTextFieldColors(),
-    hideLabel: Boolean = false
-) {
-    val softwareKeyboardController = LocalSoftwareKeyboardController.current
-
-    val textColor = textStyle.color.takeOrElse {
-        colors.textColor(enabled).value
-    }
-    val mergedTextStyle = textStyle.merge(TextStyle(color = textColor))
-    // https://issuetracker.google.com/issues/251162419
-    val contentDesc = if (hideLabel || placeholder != null) { label } else null
-    Column(
-        modifier = modifier
-            .width(IntrinsicSize.Min)
-            .wrapContentHeight()
-    ) {
-        BasicTextField(
-            value = value,
-            onValueChange = { value ->
-
-//              https://issuetracker.google.com/issues/202353328
-//              https://issuetracker.google.com/issues/225399256
-                if (keyboardOptions.keyboardType == KeyboardType.Number) {
-                    if (!value.text.contains('.')) onValueChange.invoke(value)
-                } else {
-                    onValueChange.invoke(value)
-                }
-            },
-            modifier = modifier
-                .background(colors.backgroundColor(enabled).value, MaterialTheme.shapes.small)
-                .defaultMinSize(
-                    minWidth = TextFieldDefaults.MinWidth,
-                    minHeight = TextFieldDefaults.MinHeight
-                ),
-            enabled = enabled,
-            readOnly = readOnly,
-            textStyle = mergedTextStyle,
-            cursorBrush = SolidColor(colors.cursorColor(errorMessage != null).value),
-            keyboardOptions = keyboardOptions,
-            // https://issuetracker.google.com/issues/197121898
-            keyboardActions = KeyboardActions(
-                onDone = {
-                    softwareKeyboardController?.hide()
-                }
-            ),
-            interactionSource = interactionSource,
-            singleLine = lines == 1,
-            maxLines = lines,
-            decorationBox = @Composable { innerTextField ->
-                DecorationBox(
-                    isOutlinedTextField,
-                    value,
-                    innerTextField,
-                    placeholder,
-                    if (hideLabel) null else { { Text(text = label) } },
-                    trailingIcon,
-                    lines,
-                    enabled,
-                    errorMessage,
-                    interactionSource,
-                    colors
-                )
-            }
-        )
-
-        errorMessage?.run {
-            Text(
-                text = this,
-                style = MaterialTheme.typography.caption,
-                modifier = Modifier.padding(top = 2.dp)
-            )
-        }
-    }
-}
-
-fun Modifier.modifyIf(condition: Boolean, modify: Modifier.() -> Modifier) =
-    if (condition) modify() else this
-
-fun <T> Modifier.modifyIfNotNull(modifier: T?, modify: Modifier.(T) -> Modifier): Modifier =
-    modifier?.run {
-        modify(this)
-    } ?: this
-
-
-@Composable
-private fun DecorationBox(
-    isOutlinedTextField: Boolean,
-    value: TextFieldValue,
-    innerTextField: @Composable () -> Unit,
-    placeholder: @Composable() (() -> Unit)?,
-    label: @Composable() (() -> Unit)?,
-    trailingIcon: @Composable() (() -> Unit)?,
-    lines: Int,
-    enabled: Boolean,
-    errorMessage: String?,
-    interactionSource: MutableInteractionSource,
-    colors: TextFieldColors
-) {
-    val noLabelPadding = TextFieldDefaults.textFieldWithoutLabelPadding(
-        top = 0.dp,
-        bottom = 0.dp
-    )
-    if (isOutlinedTextField) {
-        TextFieldDefaults.OutlinedTextFieldDecorationBox(
-            value = value.text,
-            visualTransformation = VisualTransformation.None,
-            innerTextField = innerTextField,
-            placeholder = placeholder,
-            label = label,
-            trailingIcon = trailingIcon,
-            singleLine = lines == 1,
-            enabled = enabled,
-            isError = errorMessage != null,
-            interactionSource = interactionSource,
-            colors = colors,
-            contentPadding = if (label == null) {
-                noLabelPadding
-            } else {
-                TextFieldDefaults.outlinedTextFieldPadding()
-            },
-            border = {
-                TextFieldDefaults.BorderBox(
-                    enabled,
-                    errorMessage != null,
-                    interactionSource,
-                    colors,
-                    MaterialTheme.shapes.small
-                )
-            }
-        )
-    } else {
-        TextFieldDefaults.TextFieldDecorationBox(
-            value = value.text,
-            visualTransformation = VisualTransformation.None,
-            innerTextField = innerTextField,
-            placeholder = placeholder,
-            label = label,
-            trailingIcon = trailingIcon,
-            singleLine = lines == 1,
-            enabled = enabled,
-            isError = errorMessage != null,
-            interactionSource = interactionSource,
-            colors = colors,
-            contentPadding = if (label == null) {
-                noLabelPadding
-            } else {
-                TextFieldDefaults.textFieldWithLabelPadding()
-            }
-        )
-    }
-}
