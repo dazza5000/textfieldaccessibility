b80d48bd77483f9876bc1e35bc6854b3d49d61c2
Darran Kelinske <darran.kelinske@well.co>
2023-03-08 09:12:00 -0600

    loading dialog with error

--- app/src/main/java/com/whereisdarran/myapplication/view/ProfileFragment.kt
+++ app/src/main/java/com/whereisdarran/myapplication/view/ProfileFragment.kt
@@ -1,12 +1,19 @@
 package com.whereisdarran.myapplication.view
 
+import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.textfield.TextInputLayout
 import com.whereisdarran.myapplication.databinding.FragmentProfileBinding
+import com.whereisdarran.myapplication.databinding.ViewLoadingBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class ProfileFragment : Fragment() {
 
@@ -27,8 +34,16 @@ class ProfileFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         with(_viewBinding!!) {
             saveButton.setOnClickListener {
-                error = !error
-                firstNameInputLayout.showError(error)
+                lifecycleScope.launch {
+                    val dialog = AlertUtil.getLoadingDialog(this@ProfileFragment.requireActivity())
+                    dialog.show()
+
+                    dialog.dismiss()
+
+
+                    error = !error
+                    firstNameInputLayout.showError(error)
+                }
             }
         }
     }
@@ -42,5 +57,15 @@ fun TextInputLayout.showError(
         this.error = "this field has an error"
     } else {
         this.error = null
+    }
+}
+
+object AlertUtil {
+    fun getLoadingDialog(context: Context): androidx.appcompat.app.AlertDialog {
+        val view = ViewLoadingBinding.inflate(LayoutInflater.from(context))
+        val dialog =
+            MaterialAlertDialogBuilder(context).setView(view.root).setCancelable(false).create()
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        return dialog
     }
 }
