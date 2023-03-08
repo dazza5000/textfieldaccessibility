7d450513328ee83d0588bab4ed2218c7561322a1
david.corrado <david.corrado@well.co>
2023-03-08 11:10:41 -0600

    Minimum code

--- app/src/main/java/com/whereisdarran/myapplication/MainActivity.kt
+++ app/src/main/java/com/whereisdarran/myapplication/MainActivity.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.semantics.LiveRegionMode
 import androidx.compose.ui.semantics.liveRegion
 import androidx.compose.ui.semantics.semantics
@@ -25,37 +24,22 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import com.whereisdarran.myapplication.ui.theme.MyApplicationTheme
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+@OptIn(DelicateCoroutinesApi::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val error = remember { mutableStateOf("") }
-            val isError = remember { mutableStateOf(false) }
             val isLoading = remember { mutableStateOf(false) }
             val value = remember { mutableStateOf(TextFieldValue("")) }
 
             MyApplicationTheme {
-                Scaffold(
-                    bottomBar = {
-                        Button({
-                            isLoading.value = true
-                            isError.value = !isError.value
-                            GlobalScope.launch {
-                                delay(500L)
-                                error.value =
-                                    if (isError.value) "please complete this field" else ""
-
-                                isLoading.value = false
-                            }
-                        }) {
-                            Text("Save")
-                        }
-                    }
-                ) {
+                Scaffold {
                     Column(modifier = Modifier.padding(it)) {
                         LoadingDialog(showLoadingDialog = isLoading.value)
                         TextField(
@@ -76,6 +60,27 @@ class MainActivity : ComponentActivity() {
                                         liveRegion = LiveRegionMode.Polite
                                     },
                             )
+                        }
+                        if(value.value.text.isNotBlank()) {
+                            Text(
+                                text = "Only blank is allowed.  You have "+ value.value.text,
+                                style = MaterialTheme.typography.caption.copy(color = Color.Red),
+                                modifier = Modifier
+                                    .padding(top = 2.dp)
+                                    .semantics(mergeDescendants = true) {
+                                        liveRegion = LiveRegionMode.Polite
+                                    },
+                            )
+                        }
+                        Button({
+                            isLoading.value = true
+                            GlobalScope.launch {
+                                delay(500L)
+                                error.value = if (error.value.isBlank()) "Name is required" else ""
+                                isLoading.value = false
+                            }
+                        }) {
+                            Text("Save")
                         }
                     }
                 }
